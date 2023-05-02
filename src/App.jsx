96fea84af4ac4d51a35ef66a13f919786b56419c
Kaizad <kaizad@email.com>
2023-05-02 13:02:29 +0200

    secret

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 
+var secrets = JSON.parse(process.env.REACT_APP_SECRET);
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Environment Variable: {process.env.REACT_APP_ENV}</p>
-        <p>Secret Variable: {process.env.REACT_APP_SECRET}</p>
+        <p>Secret Variable: {secrets.Secret1}</p>
       </header>
     </div>
   );
