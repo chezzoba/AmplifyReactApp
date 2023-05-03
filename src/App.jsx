f1093a9d85a993d25cfc3113602e414e9379ec47
Kaizad <kaizad@email.com>
2023-05-03 15:37:44 +0200

    new var

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 const reactAppVariable = process.env.REACT_APP_ENV;
 const secrets = JSON.parse(process.env.REACT_APP_SECRET);
 const amplifyEnvironmentVariable = process.env.REACT_APP_AMPLIFY_VAR;
+const ssmEnvironmentVariable = process.env.REACT_APP_SSM_VAR;
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <p>Environment Variable: {reactAppVariable}</p>
         <p>Secret Variable: {secrets.Secret1}</p>
         <p>Amplify Environment Variable: {amplifyEnvironmentVariable}</p>
+        <p>SSM Parameter: {ssmEnvironmentVariable}</p>
       </header>
     </div>
   );
